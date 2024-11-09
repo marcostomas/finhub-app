79bf848381bee8fbc405ad8c050d28cc781489ff
Marcos <mtomasferreira@hotmail.com>
2024-11-09 20:25:38 -0300

    feat: criação das páginas e das rotas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { GastosComponent } from './components/gastos/gastos.component';
+import { ExtratoComponent } from './components/extrato/extrato.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: "", pathMatch:'full' ,component: HomeComponent },
+    { path: "extrato", component: ExtratoComponent },
+    { path: "gastos", component: GastosComponent  }
+];
