504b5e3b66029379c34c672b9e4034191dfaface
lis <elisayjl@gmail.com>
2024-11-11 21:23:03 -0300

    feat: menu com efeito

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,11 +1,11 @@
+import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [RouterModule],
+  imports: [CommonModule, RouterModule],
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css'],
 })
@@ -13,4 +13,26 @@ export class HeaderComponent implements OnInit {
   constructor() {}
 
   ngOnInit() {}
+
+  activeTab: string = 'home';
+  underlinePosition: string = '0px';
+
+  setActiveTab(tab: string): void {
+    this.activeTab = tab;
+    this.updateUnderlinePosition(tab);
+  }
+
+  updateUnderlinePosition(tab: string): void {
+    switch (tab) {
+      case 'home':
+        this.underlinePosition = '0px';
+        break;
+      case 'extrato':
+        this.underlinePosition = '100px';
+        break;
+      case 'gastos':
+        this.underlinePosition = '200px';
+        break;
+    }
+  }
 }
